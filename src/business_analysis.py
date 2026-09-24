ffaf84d4d04f3390fda706a392ab1b857689c469
Hanbiao Mao <157418644+hfm5311@users.noreply.github.com>
2026-09-24 18:07:31 -0500

    Add product category analysis

--- src/business_analysis.py
+++ src/business_analysis.py
@@ -1,15 +1,4 @@
-# -----------------------------
-# Business Insights
-# -----------------------------
 
-# 1. November 2017 had the highest monthly revenue,
-#    reaching approximately 1.01 million.
-#    Revenue also remained relatively high from January to May 2018.
-
-# 2. The average order value (AOV) was approximately 137.75.
-
-# 3. SP generated the highest total revenue among all customer states,
-#    with approximately 5.20 million in revenue.
 
 import pandas as pd
 
@@ -145,3 +134,79 @@ state_revenue =customer_revenue.groupby("customer_state").agg(
 ).reset_index()
 print(state_revenue.sort_values("state_revenue",ascending=False).head(10))
 
+# -----------------------------
+# Business Insights
+# -----------------------------
+
+# 1. November 2017 had the highest monthly revenue,
+#    reaching approximately 1.01 million.
+#    Revenue also remained relatively high from January to May 2018.
+
+# 2. The average order value (AOV) was approximately 137.75.
+
+# 3. SP generated the highest total revenue among all customer states,
+#    with approximately 5.20 million in revenue.
+
+
+
+# -----------------------------
+# Product & Category Analysis
+# -----------------------------
+products = pd.read_csv(
+    "data/raw/olist_products_dataset.csv"
+)
+print(products.columns)
+print(products.head())
+
+product_item = pd.merge(
+    order_items,
+    products,
+    on = "product_id",
+    how = "inner"
+)
+print(product_item.head())
+
+product_category_count = (product_item.groupby("product_category_name").agg(
+    item_count = ("product_id", "count")
+).reset_index())
+print(product_category_count.sort_values("item_count", ascending=False).head(10))
+
+# print(product_item.columns)
+
+# Group by product category and calculate total revenue
+category_revenue = (
+    product_item.groupby("product_category_name")
+    .agg(
+        category_revenue = ("price", "sum")
+    )
+    .reset_index()
+)
+print(
+    category_revenue
+    .sort_values("category_revenue", ascending=False)
+    .head(10)
+)
+
+# Merge product category sales count and revenue into one summary table
+category_summary = pd.merge(
+    product_category_count,
+    category_revenue,
+    on="product_category_name",
+    how="inner"
+)
+print(category_summary.head())
+# Calculate the average revenue generated per item for each product category
+category_summary["avg_revenue_per_item"] = category_summary["category_revenue"] / category_summary["item_count"]
+print(
+    category_summary.sort_values("avg_revenue_per_item",ascending=False)
+    .head(10)
+)
+# -----------------------------
+# Business Insights
+# -----------------------------
+
+# 1. cama_mesa_banho sold the most items among all product categories.
+
+# 2. beleza_saude generated the highest total revenue among all product categories.
+
+# 3. pcs had the highest average revenue per item.
